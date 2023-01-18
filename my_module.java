c1df0440707361fa5c4ef714e4fe279fe07a0053
jongwooooo <jongwoo3768@naver.com>
2023-01-18 16:11:25 +0900

    Add my_module [Add&Sub]

---
+++ my_module.java
@@ -0,0 +1,65 @@
+public class my_module {
+
+
+
+	public static void main(String[] args) {
+
+
+
+		int x = 9;
+
+
+
+		int y = 5;
+
+
+
+		int add = x+y;
+
+
+
+		//int sub = x-y;
+
+
+
+		//int mult = x*y;
+
+
+
+		//int div = x/y;
+
+
+
+		//int mod = x%y;
+
+
+
+		
+
+
+
+		System.out.println(add);
+
+
+
+		//System.out.println(sub);
+
+
+
+		//System.out.println(mult);
+
+
+
+		//System.out.println(div);
+
+
+
+		//System.out.println(mod);
+
+
+
+	}
+
+
+
+}
